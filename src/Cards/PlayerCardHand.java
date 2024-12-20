28b79c12e650f01f91d3ddcaa782c3a09ff9f7bd
AnaBeCosta <anacost.a@outlook.com>
2024-12-20 20:45:39 -0300

    Commit teste

--- src/Cards/PlayerCardHand.java
+++ src/Cards/PlayerCardHand.java
@@ -13,24 +13,25 @@ public class PlayerCardHand extends CardHand {
 	 * @return If the card was added or not successfully.
 	 */
 	public boolean add(Card card) {
-		boolean cardAdded = false;
+		if(isBust() || hasBlackJack()){
+			return false
+		}
 
-		if (!isBust() && !hasBlackjack()) {
-			cardAdded = super.add(card);
+		boolean cardAdded = super.add(card);
 
-			// if bust, check if all Aces are set to 1, if not, do it!
-			if (isBust()) {
-				for (Card eachCard : this) {
-					eachCard.getFace().switchAce(); // switch ace to low 1 switching...
-
-					if (!isBust()) // if hand isn't bust anymore, then stop switching
-					{
-						break; // needed to break out of loop to stop switching aces
-					}
-				}
-			}
+		if (isBust()) {
+			adJustAces();
 		}
 
 		return cardAdded;
+	}
+
+	private void adJustAces(){
+		for (Card eachCard : this) {
+			eachCard.getFace().switchAce();
+			if (!isBust()) {
+				break;
+			}
+		}
 	}
 }
